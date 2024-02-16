d1ea92708fa3a1e3e34a4f76030e3b311739f0ac
Adesh Jadhav <adejad1234@gmail.com>
2024-02-16 16:19:48 +0530

    Added lots of content

--- Javascript/useMe.js
+++ Javascript/useMe.js
@@ -1,20 +1,8 @@
-const arr1 = ["Adesh ", "Pramod ", "Jadhav"];
-const arr2 = [10, 20, 30];
-
-const name = arr1.reduce((accumulator, curElement)=>{
-  return accumulator += curElement;
-}, "Name: ");
+const arr = {
+  fname: 'Adesh'
+}
 
 
-const add = arr2.reduce((accumulator, curElement)=>{
-  return accumulator += curElement;
-}, 0);
+const { fname: name} = arr;
 
-const mul = arr2.reduce((accumulator, curElement)=>{
-  return accumulator *= curElement;
-}, 1);
-
-
-
-console.log(mul);
-
+console.log(fname);
