f3e0aa8c96a6f0ddb31673d72957721d9edcfef9
Adesh Jadhav <adejad1234@gmail.com>
2024-07-01 09:52:03 +0530

    Added Next Request

--- Javascript/useMe.js
+++ Javascript/useMe.js
@@ -1,14 +1,2 @@
-function calculateVowels (str){
-  let vowels = 'aeiou';
-  let count = 0;
-  for(let ch of str){
-    if(vowels.includes(ch.toLowerCase())){
-      count++;
-    }
-  }
-
-  console.log('count: ', count);
-}
-
-
-calculateVowels('Adesh')
+console.log("1st value" || "2nd value");
+console.log("" || "");
