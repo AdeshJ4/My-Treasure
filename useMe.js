09849e5d9518469b580fe3f74ea169e206d13c7e
Adesh Jadhav <adejad1234@gmail.com>
2025-03-21 20:18:18 +0530

    qa

--- useMe.js
+++ useMe.js
@@ -1,18 +1,138 @@
-function compressString(str) {
-    let compressed = "";
-    let count = 1;  // Start counting from 1
-
-    for (let i = 0; i < str.length; i++) {
-        if (i < str.length - 1 && str[i] === str[i + 1]) {
-            count++; // Increase count if next char is the same
-        } else {
-            compressed += str[i] + count; // Append char and its count
-            count = 1; // Reset count for next char
-        }
-    }
-
-    return compressed.length < str.length ? compressed : str; // Return shorter string
+function fetchData() {
+    return new Promise((resolve, reject) => {
+        resolve({ id: 101, name: 'Adesh' })
+    });
 }
 
-// Test Cases
-console.log(compressString("aaabbaacddd"));
+
+
+fetchData()
+    .then((data) => console.log(data)
+    )
+    .catch((error) => console.log(error.message))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function getUser(id, callback) {
+//     console.log(`getting user ${id}`);
+
+//     setTimeout(() => {
+//         callback({ id: 101, github_name: 'AdeshJ4' });
+//     }, 2000);
+// }
+// function handleUserData(userData) {
+//     getRepos(userData.id, handleReps)
+// }
+
+// function getRepos(userId, callback) {
+//     console.log('getting repos for ', userId);
+
+//     setTimeout(() => {
+//         callback(["repo_1", "repo_2"]);
+//     }, 2000)
+// }
+
+// function handleReps(repos) {
+//     getCommits(repos[1], handleCOmmitsData)
+// }
+
+// function getCommits(repo, callback) {
+//     console.log(`getting commits for ${repo}`);
+
+//     setTimeout(() => {
+//         callback(['commit_1', 'commit_2', 'commit_3'])
+//     }, 2000);
+// }
+
+// function handleCOmmitsData(commits) {
+//     console.log(commits);
+// }
+
+// getUser(101, handleUserData);
