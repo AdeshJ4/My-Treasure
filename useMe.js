bc21751698c9aae9f2ca1d8fa5e76f7d46b89a52
Adesh Jadhav <adejad1234@gmail.com>
2025-03-30 13:59:36 +0530

    a

--- useMe.js
+++ useMe.js
@@ -1,12 +1,30 @@
-const fs = require('fs');
+const http = require('http');
 
+const server = http.createServer((req, res) => {
+  if (req.method === 'POST') {
+    let body = "";
 
-fs.readdir('../../', (err, data) => {
-  if (err)
-    console.log(err);
-  else
-    console.log(data);
+    req.on("data", (chunk) => {
+      body += chunk.toString();
+    })
 
-
+    req.on("end", () => {
+      console.log(body), body;
+      res.end("Data received successfully")
+    })
+  } else if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end("Home Page");
+  } else if (req.url === '/api/customer') {
+    res.writeHead(200, { 'Content-Type': 'application/json' })
+    res.end(JSON.stringify({ name: 'Adesh', age: 22 }))
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end("404 Not Found")
+  }
 })
 
+
+server.listen(5000, () => {
+  console.log(`Server ius listening on port 5000`)
+})
